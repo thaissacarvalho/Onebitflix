da3eee2d5815aaba03ed5111da38e2b36e02409a
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2023-12-13 09:26:32 -0300

    create a admin file.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express from "express";
 import { sequelize } from "./database/index";
+import { adminJs, adminJsRouter } from "./adminjs";
 
 const app = express();
+
+app.use(express.static('public'));
+
+app.use(adminJs.options.rootPath, adminJsRouter);
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
